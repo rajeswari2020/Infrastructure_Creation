236c57dcdb17a46043897f38fd28e42b3e5b6247
rajeswari2020 <60647623+rajeswari2020@users.noreply.github.com>
2026-03-12 13:04:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "demo-terraform-eks-state-bucket-sample-demo2"
+    bucket         = demo-terraform-eks-state-bucket-sample-demo-12032026"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "demo-terraform-eks-state-locks"
